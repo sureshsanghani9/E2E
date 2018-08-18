95a3e2b8e209bc4816900f8c4c7ff2011f5f8c5b
sureshsanghani <sureshkumar.sanghani@wu.com>
2018-08-18 12:58:55 +0530

    Send Invitation Pages

--- E2E/E2E/Controllers/EmployerAdminController.cs
+++ E2E/E2E/Controllers/EmployerAdminController.cs
@@ -1,7 +1,10 @@
 ﻿using E2EInfrastructure.Helpers;
 using E2ERepositories.Interface;
+using E2EViewModals.Invitations;
+using E2EViewModals.User;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -83,6 +86,62 @@ namespace E2E.Controllers
             }
 
 
+        }
+
+        public ActionResult ManageEmployee()
+        {
+            return View();
+        }
+
+        public ActionResult ManageAdmin()
+        {
+            return View();
+        }
+
+        public ActionResult Reports()
+        {
+            return View();
+        }
+
+        public ActionResult Subscription()
+        {
+            return View();
+        }
+
+
+        public JsonResult SendInvitations(InvitationsViewModal Invitations)
+        {
+            var user = (UserViewModal)Session["User"];
+            if (Invitations == null || Invitations.Invites == null)
+            {
+                return Json(new { Code = 0, Message = "Something wrong is occured. Please try after sometime!" });
+            }
+            else
+            {
+                foreach (Invite invite in Invitations.Invites)
+                {
+                    string code = EncryptionHelper.Encrypt(invite.FirstName + "||" + invite.LastName + "||" + invite.Role);
+                    SendInvitationEmail(invite.Email, invite.AdditionalNotes, user.BusinessName, code);
+                }
+
+                return Json(new { Code = 1, Message = "Invitations are sent successfully!" });
+            }
+            
+        }
+
+        private void SendInvitationEmail(string Email, string AdditionalNotes, string BusinessName, string Code)
+        {
+            string baseURL = string.Format("{0}://{1}{2}", Request.Url.Scheme, Request.Url.Authority, Url.Content("~"));
+            string subject = "Create your new E2EWebPortal Login Profile.";
+            String emailBody = "Hello, <br/><br/> Please click on following link/button to create New Login profile and password  for E2EWebPortal. <br/>"
+                + "Link : <a href='" + baseURL + "/User/SignUp?code=" + Code + "'>Click here</a> <br/><br/>"
+                + (!string.IsNullOrEmpty(AdditionalNotes) ? "Additional Notes : " + AdditionalNotes + "<br/><br/> " : "")
+                + "Regards, <br/> " + BusinessName + " ";
+
+
+
+            string From = ConfigurationManager.AppSettings["FromEmail"] != null ? ConfigurationManager.AppSettings["FromEmail"].ToString() : "";
+            EmailHelper.SendEmail(From, Email, subject, emailBody, null, "", true);
         }
 
     }
